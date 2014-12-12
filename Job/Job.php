2e4deff3d1204f71fa4a2c41e24518dddaf89996
Fabrice LEBON <fabrice.lebon@tessi.fr>
2014-12-12 22:29:41 +0100

    Confusion correction between ContainerAwareInterface and ContainerInterface

--- Job/Job.php
+++ Job/Job.php
@@ -2,27 +2,32 @@
 
 namespace Tessi\JobBundle\Job;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-abstract class Job implements JobInterface, ContainerAwareInterface
+abstract class Job implements ContainerAwareInterface, JobInterface
 {
-	public function __construct(ContainerAwareInterface $container)
+	public function __construct(ContainerInterface $container = NULL)
 	{
 		$this->setContainer($container);
 	}
 
 	public function createTasks()
 	{
-
 	}
 
 	public function executeTask($input, $scriptNamespace)
 	{
-		
 	}
 
 	protected function getContainer()
 	{
 		return $this->container;
 	}
+
+	public function setContainer(ContainerInterface $container = NULL)
+	{
+		$this->container = $container;
+	}
+
 }
